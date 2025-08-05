b6e6da3915a4394b2efe86af1dd1e1b68f9b02bd
alfarreca <alfarrecas@proton.me>
2025-08-05 12:13:55 +0100

    Update hwcrypto_dxcc.py

--- hwcrypto_dxcc.py
+++ hwcrypto_dxcc.py
@@ -7,7 +7,7 @@
 import logging
 import hashlib
 from struct import pack
-from Cryptodome.Util.number import bytes_to_long
+from Crypto.Util.number import bytes_to_long
 from Cryptodome.Cipher import AES
 from Cryptodome.Util import Counter
 from mtkclient.Library.utils import LogBase, logsetup
@@ -1542,3 +1542,4 @@ if __name__ == "__main__":
     desc = hw_desc_set_cipher_config1(desc, SepHashMode.SEP_HASH_SHA256)
     desc = hw_desc_set_setup_mode(desc, SetupOp.SETUP_WRITE_STATE1)
     print(desc)
+
